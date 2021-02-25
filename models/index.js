a03c44bef04a4e4b3d4bbf3354b9f50bb103cb48
JinseongHwang <eddy5360@naver.com>
2021-02-25 16:18:54 +0900

    feat: 즐겨찾기 모델 생성

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Brand = require('./Brand.js');
 const Category = require('./Category.js');
 const Promotion = require('./Promotion.js');
 const User = require('./User.js');
+const Favorite = require('./Favorite.js');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
@@ -16,13 +17,16 @@ db.Brand = Brand;
 db.Category = Category;
 db.Promotion = Promotion;
 db.User = User;
+db.Favorite = Favorite;
 
 Brand.init(sequelize);
 Category.init(sequelize);
 Promotion.init(sequelize);
 User.init(sequelize);
+Favorite.init(sequelize);
 
 Brand.associate(db);
 Category.associate(db);
+Favorite.associate(db);
 
 module.exports = db;
