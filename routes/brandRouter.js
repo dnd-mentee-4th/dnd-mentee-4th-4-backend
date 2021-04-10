d645c762671a3ce0c44a8f34eb4a657be8860450
JinseongHwang <eddy5360@naver.com>
2021-04-10 19:09:41 +0900

    fix: 브랜드 검색 라우터 제거

--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -9,10 +9,4 @@ router.get('/brands', async (req, res) => {
   res.status(200).send(brands);
 });
 
-router.get('/brands/search', async (req, res) => {
-  const brands = await brandService.searchByName(req.query.name);
-
-  res.status(200).send(brands);
-});
-
 module.exports = router;
