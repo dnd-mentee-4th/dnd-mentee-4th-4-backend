b8d74e02c6ef016ebabc0d61fb923d5d355dee56
JinseongHwang <eddy5360@naver.com>
2021-04-13 16:44:11 +0900

    fix: batch 작업 브랜드별 간격 조절 (#72)
    * chore: Install user-agents module
    
    * fix: batch 작업 브랜드별 간격 조절

--- modules/cron.js
+++ modules/cron.js
@@ -1,11 +1,53 @@
 const cron = require('node-cron');
-const executeCrawler = require('../crawler/executeCrawler');
+const {
+  eightSecondsSaveAll,
+} = require('../crawler/clothes/eightSecondsCrawler');
+const { musinsaSaveAll } = require('../crawler/clothes/musinsaCrawler');
+const { styleShareSaveAll } = require('../crawler/clothes/styleShareCrawler');
+const { coupangSaveAll } = require('../crawler/socialCommerce/coupangCrawler');
+const {
+  eleventhStreetSaveAll,
+} = require('../crawler/socialCommerce/eleventhStreetCrawler');
+const { gmarketSaveAll } = require('../crawler/socialCommerce/gmarketCrawler');
+const { tmonSaveAll } = require('../crawler/socialCommerce/tmonCrawler');
+const {
+  wemakepriceSaveAll,
+} = require('../crawler/socialCommerce/wemakepriceCrawler');
+// const executeCrawler = require('../crawler/executeCrawler');
 
 const batch = () => {
-  // 매일 04:00에 Crawling 작업을 실시한다.
-  cron.schedule('0 4 * * *', async () => {
-    await executeCrawler.run();
-  });
+  /**
+   * 매일 04:00에 Crawling 작업을 실시한다.
+   * {1, 2, 3}, {4, 5}, {6, 7, 8} : 5분 간격으로 크롤링을 실시한다.
+   */
+
+  const START_HOUR = 4; // 0 ~ 23
+  const START_MINUTE = 0; // 0 ~ 59
+  const INTERVAL = 5; // crawling interval (minutes)
+
+  cron.schedule(
+    `${START_MINUTE + INTERVAL * 0} ${START_HOUR} * * *`,
+    async () => {
+      await musinsaSaveAll();
+      await styleShareSaveAll();
+      await eightSecondsSaveAll();
+    },
+  );
+  cron.schedule(
+    `${START_MINUTE + INTERVAL * 1} ${START_HOUR} * * *`,
+    async () => {
+      await coupangSaveAll();
+      await eleventhStreetSaveAll();
+    },
+  );
+  cron.schedule(
+    `${START_MINUTE + INTERVAL * 2} ${START_HOUR} * * *`,
+    async () => {
+      await gmarketSaveAll();
+      await tmonSaveAll();
+      await wemakepriceSaveAll();
+    },
+  );
 };
 
 module.exports = {
